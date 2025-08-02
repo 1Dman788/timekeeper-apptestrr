a83208ac584887cf22a3f3fdd9d5f8b803bde23f
1Dman788 <mr.jeffboyyes@gmail.com>
2025-08-02 17:58:30 -0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,32 +1,25 @@
 // Firebase initialization module
-// Imports required functions from the Firebase SDKs. See docs for more details
-// on using the modular Web SDK: https://firebase.google.com/docs/web/learn-more#modular-api
 
+// Import required Firebase functions
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-// Configuration provided by the user. Measurement ID is optional.
+// Your Firebase project config
 const firebaseConfig = {
-  apiKey: "AIzaSyBcIBVNGyy8Bw_9TYJx7hviM0Z1LeSd22E",
-  authDomain: "timekeep2test.firebaseapp.com",
-  projectId: "timekeep2test",
-  storageBucket: "timekeep2test.firebasestorage.app",
-  messagingSenderId: "608331276389",
-  appId: "1:608331276389:web:6b77bd26e63699d88c268c",
-  measurementId: "G-S9JE53BMN3",
+  apiKey: "AIzaSyDU7yhApAh4EbZYmIvqdefLy7xY6G2Ik7g",
+  authDomain: "uhhh-27210.firebaseapp.com",
+  projectId: "uhhh-27210",
+  storageBucket: "uhhh-27210.firebasestorage.app",
+  messagingSenderId: "869810449235",
+  appId: "1:869810449235:web:618ab8dadce7ea93490fa6",
+  measurementId: "G-9335TS6XZM"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Optional analytics initialization. We call this but ignore the returned value
-// since analytics are not required for core functionality.
-getAnalytics(app);
-
-// Initialize services
-const auth = getAuth(app);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-export { app, auth, db };
+// Export app + database
+export { app, db };
